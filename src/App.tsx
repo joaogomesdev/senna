eb40b8641c317c3ee4f5354913314b6e74179748
Joao Gomes <joaopfg.2002@gmail.com>
2022-06-21 00:14:42 +0100

    Apollo Client and GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import * as React from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    id
+    title
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
-  return <h1>Hello Worlds </h1>;
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+  console.log(data);
+
+  if (!data) {
+    return <h1>No lessons</h1>;
+  }
+
+  return (
+    <ul>
+      {data.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
+  );
 }
 
 export default App;
